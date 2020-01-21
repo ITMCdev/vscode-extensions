fcc0ed2b539d8347c856756be0ff3ba454eff8d1
Dragos Cirjan <dragos.cirjan@cellebrite.com>
2020-01-21 09:09:14 +0200

    +

--- publish.sh
+++ publish.sh
@@ -2,7 +2,11 @@
 
 find . -mindepth 1 -maxdepth 1 -type d -not -iname "*.*" | while read d; do
     cd $d;
-    echo "publishing itmcdev-$d..."
-    vsce publish;
+    echo "publishing itmcdev-$d...i"
+    if [[ "$VSCE_TOKEN" != "" ]]; then
+        vsce publish -p $VSCE_TOKEN;
+    else
+	vsce publish;
+    fi
     cd ..;
 done
