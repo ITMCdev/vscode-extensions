0e3d22a22802996804f0e3e90b6c96a04c22debb
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-03 23:06:25 +0300

    post commit hook

--- .scripts/post-commit.sh
+++ .scripts/post-commit.sh
@@ -8,7 +8,7 @@ set -eu
 OUTPUT_FILE=CHANGELOG.md
 
 # generate the changelog
-git --no-pager log --no-merges --format="### %s%d%n>%aD%n%n>Author: %aN (%aE)%n%n>Commiter: %cN (%cE)%n%n%b%n%N%n" > $OUTPUT_FILE
+git --no-pager log --no-merges --format="### %s%d%n>%aD%n%n>Author: %aN (%aE)%n%n>Commiter: %cN (%cE)%n%n%b%n%N%n" #> $OUTPUT_FILE
 
 # prevent recursion!
 # since a 'commit --amend' will trigger the post-commit script again
